830a522e5b2983ec341617ff6ee5a7fb1b448bab
monpro <monproshen@gmail.com>
2022-08-20 22:42:49 +1000

    make Chunk carry with a constants array and add constants to it

--- src/chunk.h
+++ src/chunk.h
@@ -2,6 +2,8 @@
 #define MON_C_INTERPRETER_CHUNK_H
 
 #include "common.h"
+#include "value.h"
+
 typedef enum {
     OP_RETURN
 } OpCode;
@@ -10,10 +12,12 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+int addConstant(Chunk* chunk, Value value);
 
 #endif //MON_C_INTERPRETER_CHUNK_H
