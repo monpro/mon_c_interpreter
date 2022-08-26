4dd4eaa06d3ffbbf63887019775f5b56c1895d9c
monpro <monproshen@gmail.com>
2022-08-26 18:27:37 +1000

    add interpreter pointer in VM and interpret basic logic

--- src/vm.c
+++ src/vm.c
@@ -5,6 +5,8 @@
 // Single Global VM Object
 VM vm;
 
+InterpretResult run();
+
 void initVM() {
 
 }
@@ -12,3 +14,23 @@ void initVM() {
 void freeVM() {
 
 }
+
+InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+    for (;;) {
+        uint8_t instruction;
+        switch (instruction = READ_BYTE()) {
+            case OP_RETURN:
+                return INTERPRET_OK;
+        }
+    }
+#undef READ_BYTE
+}
+
+
+InterpretResult interpret(Chunk* chunk) {
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
+
--- src/vm.h
+++ src/vm.h
@@ -5,9 +5,17 @@
 
 typedef struct {
     Chunk* chunk;
+    // instruction pointer
+    uint8_t* ip;
 } VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
 
 void initVM();
 void freeVM();
-
+InterpretResult interpret(Chunk* chunk);
 #endif //MON_C_INTERPRETER_VM_H
