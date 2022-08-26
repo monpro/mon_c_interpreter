d7696b83e9338c5e635e3250e4fbc727694b98f1
monpro <monproshen@gmail.com>
2022-08-26 11:36:41 +1000

    add line information of bytes to chunk

--- src/chunk.c
+++ src/chunk.c
@@ -6,21 +6,25 @@ void initChunk(Chunk* chunk) {
     chunk->capacity = 0;
     chunk->count = 0;
     chunk->code = NULL;
+    chunk->lines = NULL;
     initValueArray(&chunk->constants);
 }
 
-void writeChunk(Chunk* chunk, uint8_t byte) {
+void writeChunk(Chunk* chunk, uint8_t byte, int line) {
  if (chunk->capacity < chunk->count + 1) {
      int capacity = chunk->capacity;
      chunk->capacity = GROW_CAPACITY(capacity);
      chunk->code = GROW_ARRAY(uint8_t, chunk->code, capacity, chunk->capacity);
+     chunk->lines = GROW_ARRAY(int, chunk->lines, capacity, chunk->capacity);
  }
  chunk->code[chunk->count] = byte;
+ chunk->lines[chunk->count] = line;
  chunk->count++;
 }
 
 void freeChunk(Chunk* chunk)  {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->capacity);
     freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
--- src/main.c
+++ src/main.c
@@ -5,10 +5,10 @@ int main() {
     initChunk(&chunk);
 
     int constant = addConstant(&chunk, 1.5);
-    writeChunk(&chunk, OP_CONSTANT);
-    writeChunk(&chunk, constant);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
 
-    writeChunk(&chunk, OP_RETURN);
+    writeChunk(&chunk, OP_RETURN, 123);
     disassembleChunk(&chunk, "test");
     freeChunk(&chunk);
     return 0;
