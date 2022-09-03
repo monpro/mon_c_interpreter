74d66370c575453fd65c9dbf1805c27a94274dbe
monpro <monproshen@gmail.com>
2022-09-03 14:16:09 +1000

    add error handling & panicModel at compiler

--- src/compiler.c
+++ src/compiler.c
@@ -5,12 +5,29 @@
 typedef struct {
     Token current;
     Token previous;
+    // whether error happened during compilation
+    bool hadError;
+    // after first met error, we enter into panicMode
+    bool panicMode;
 } Parser;
 
 Parser parser;
 
 void errorAt(Token *token, const char *msg) {
-    //TODO
+    if (parser.panicMode == true) {
+        return;
+    }
+    parser.panicMode = true;
+    fprintf(stderr, "[line %d] Error", token->line);
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, "at end");
+    } else if (token->type == TOKEN_ERROR) {
+
+    } else {
+        fprintf(stderr, "at '%.*s'", token->length, token->start);
+    }
+    fprintf(stderr, ": %s\n", msg);
+    parser.hadError = true;
 }
 
 void error(const char *msg) {
@@ -26,11 +43,21 @@ void advance() {
     }
 }
 
-// TODO: add advance, expression and consume
-void compile(const char* source, Chunk* chunk) {
+// TODO: add expression and consume
+void consume(TokenType type, const char *msg) {
+    if (parser.current.type == type) {
+        advance();
+        return;
+    }
+    error(msg);
+}
+
+bool compile(const char* source, Chunk* chunk) {
+    parser.panicMode = false;
+    parser.hadError = false;
     initScanner(source);
-    //advance();
+    advance();
     //expression();
-    //consume(TOKEN_EOF, "Expect the end of expression");
-    return;
+    consume(TOKEN_EOF, "Expect the end of expression");
+    return !parser.hadError;
 }
