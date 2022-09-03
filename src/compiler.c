bfa1216f0cdb0b12efb36b985fe66fbba02fd958
monpro <monproshen@gmail.com>
2022-09-03 14:09:18 +1000

    add Parser & its advance logic at compiler

--- src/compiler.c
+++ src/compiler.c
@@ -2,6 +2,30 @@
 #include "scanner.h"
 #include "chunk.h"
 
+typedef struct {
+    Token current;
+    Token previous;
+} Parser;
+
+Parser parser;
+
+void errorAt(Token *token, const char *msg) {
+    //TODO
+}
+
+void error(const char *msg) {
+    errorAt(&parser.current, msg);
+}
+
+void advance() {
+    parser.previous = parser.current;
+    for (;;) {
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
+        error(parser.current.start);
+    }
+}
+
 // TODO: add advance, expression and consume
 void compile(const char* source, Chunk* chunk) {
     initScanner(source);
