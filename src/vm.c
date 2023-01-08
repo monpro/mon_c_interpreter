dc64415f31cff41ee2dac2943b62d5f7b42c8a80
monpro <monproshen@gmail.com>
2023-01-08 13:31:48 +1100

    add a native function clock

--- src/vm.c
+++ src/vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+#include <time.h>
 #include "common.h"
 #include "vm.h"
 #include "debug.h"
@@ -12,6 +13,10 @@
 
 // Single Global VM Object
 VM vm;
+
+static Value clockNative(int argCount, Value* args) {
+    return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
+}
 
 void resetStack() {
     vm.stackTop = vm.stack;
@@ -44,11 +49,21 @@ static void runTimeError(const char* format, ...) {
     resetStack();
 }
 
+static void defineNative(const char* name, NativeFn function) {
+    push(OBJ_VAL(copyString(name, (int) strlen(name))));
+    push(OBJ_VAL(newNative(function)));
+    tableSet(&vm.globals, AS_STRING(vm.stack[0]), vm.stack[1]);
+    pop();
+    pop();
+}
+
 void initVM() {
     resetStack();
     vm.objects = NULL;
     initTable(&vm.globals);
     initTable(&vm.strings);
+
+    defineNative("clock", clockNative);
 }
 
 void freeVM() {
@@ -100,6 +115,13 @@ static bool callValue(Value callee, int count) {
         switch (OBJ_TYPE(callee)) {
             case OBJ_FUNCTION:
                 return call(AS_FUNCTION(callee), count);
+            case OBJ_NATIVE: {
+                NativeFn native = AS_NATIVE(callee);
+                Value result = native(count, vm.stackTop - count);
+                vm.stackTop -= count + 1;
+                push(result);
+                return true;
+            }
             default:
                 break;
         }
